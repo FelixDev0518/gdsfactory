714133190632b3f867661c41ae590f602201c575
Yoseob <yoseob.shim@rwth-aachen.de>
2026-06-16 19:02:15 +0200

    feat: add test_custom_layouts.py - Add test pipeline for photonic cable - Add test pipeline for electrode ports

---
+++ practice/test_custom_layouts.py
@@ -0,0 +1,32 @@
+import pytest
+import gdsfactory as gf
+
+import tests.components.test_components as tc
+
+from layouts.Photomodulator import (
+    photonic_cable,
+    cold_electrode,
+    warm_electrode,
+    graphene_under,
+    graphene_upper
+)
+
+
+
+def test_photonic_cable_creation():
+    c = photonic_cable(width=5.0, length=20.0)
+    
+    # Is component empty?
+    assert len(c.polygons) > 0
+    # Is there polygon?
+    assert (1, 0) in c.layers
+
+
+def test_electrode_ports():
+    c = cold_electrode(width=3.0, length=3.0)
+    
+    assert "junction_graphene" in c.ports
+
+    assert c.ports["junction_graphene"].width == 3.0
+
+
